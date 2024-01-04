2c939c4e1533d3bad0953252f2421a218313147a
yulog <29723448+yulog@users.noreply.github.com>
2024-01-04 22:02:53 +0900

    comment

--- server/handler.go
+++ server/handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/yulog/mi-diary/model"
 )
 
+// IndexHandler は/のハンドラ
 func (srv *Server) IndexHandler(c echo.Context) error {
 	var reactions []model.Reaction
 	srv.app.DB().
@@ -22,6 +23,7 @@ func (srv *Server) IndexHandler(c echo.Context) error {
 	return renderer(c, cm.Index("index", cm.Reaction(reactions), cm.HashTag(tags)))
 }
 
+// ReactionsHandler は/reactions/:nameのハンドラ
 func (srv *Server) ReactionsHandler(c echo.Context) error {
 	name := c.Param("name")
 	var notes []model.Note
@@ -33,12 +35,14 @@ func (srv *Server) ReactionsHandler(c echo.Context) error {
 	return renderer(c, cm.Note(name, notes))
 }
 
+// HashTagsHandler は/hashtags/:nameのハンドラ
 func (srv *Server) HashTagsHandler(c echo.Context) error {
 	name := c.Param("name")
 	var notes []model.Note
 	srv.app.DB().
 		NewSelect().
 		Model((*model.NoteToTag)(nil)).
+		// 必要な列だけ選択して、不要な列をなくす
 		Relation("Note", func(q *bun.SelectQuery) *bun.SelectQuery {
 			return q.Column("id", "user_id", "reaction_name")
 		}).
