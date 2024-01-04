b3573c7c2b2432aa45fca3359e6efc24dbe56bec
yulog <29723448+yulog@users.noreply.github.com>
2024-01-04 21:38:49 +0900

    refactor: ファイルを分ける

---
+++ server/handler.go
@@ -0,0 +1,52 @@
+package server
+
+import (
+	"github.com/labstack/echo"
+	"github.com/uptrace/bun"
+	cm "github.com/yulog/mi-diary/components"
+	"github.com/yulog/mi-diary/model"
+)
+
+func (srv *Server) IndexHandler(c echo.Context) error {
+	var reactions []model.Reaction
+	srv.app.DB().
+		NewSelect().
+		Model(&reactions).
+		Scan(c.Request().Context())
+	var tags []model.HashTag
+	srv.app.DB().
+		NewSelect().
+		Model(&tags).
+		Scan(c.Request().Context())
+	// return c.HTML(http.StatusOK, fmt.Sprint(reactions))
+	return renderer(c, cm.Index("index", cm.Reaction(reactions), cm.HashTag(tags)))
+}
+
+func (srv *Server) ReactionsHandler(c echo.Context) error {
+	name := c.Param("name")
+	var notes []model.Note
+	srv.app.DB().
+		NewSelect().
+		Model(&notes).
+		Where("reaction_name = ?", name).
+		Scan(c.Request().Context())
+	return renderer(c, cm.Note(name, notes))
+}
+
+func (srv *Server) HashTagsHandler(c echo.Context) error {
+	name := c.Param("name")
+	var notes []model.Note
+	srv.app.DB().
+		NewSelect().
+		Model((*model.NoteToTag)(nil)).
+		Relation("Note", func(q *bun.SelectQuery) *bun.SelectQuery {
+			return q.Column("id", "user_id", "reaction_name")
+		}).
+		Relation("HashTag", func(q *bun.SelectQuery) *bun.SelectQuery {
+			return q.Column("")
+		}).
+		Column("").
+		Where("hash_tag.text = ?", name).
+		Scan(c.Request().Context(), &notes)
+	return renderer(c, cm.Note(name, notes))
+}
