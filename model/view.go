9d96968caf9e27e4b93ab5f276f9626d8ea00e59
yulog <29723448+yulog@users.noreply.github.com>
2024-04-27 16:31:00 +0900

    fix: 不要な部分の削除

--- model/view.go
+++ model/view.go
@@ -1,26 +1,8 @@
 package model
 
-import (
-	"time"
-)
-
 type Archive struct {
 	YM       string
 	YmCount  int64
 	YMD      string
 	YmdCount int64
-}
-
-type DisplayNote struct {
-	ID           string `bun:",pk"`
-	UserID       string `bun:",pk"`
-	UserName     string
-	DisplayName  string
-	AvatarURL    string
-	ReactionName string
-	Text         string
-	CreatedAt    time.Time
-	User         User      `bun:"rel:belongs-to,join:user_id=id"`
-	Reaction     Reaction  `bun:"rel:belongs-to,join:reaction_name=name"`
-	Tags         []HashTag `bun:"m2m:note_to_tags,join:Note=HashTag"`
 }
