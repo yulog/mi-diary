74668cc5c8f872dda6396f0bf56e8aa1adc9f7b9
yulog <29723448+yulog@users.noreply.github.com>
2024-06-09 17:43:13 +0900

    refactor: router

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"net"
 
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/yulog/mi-diary/app"
 	"github.com/yulog/mi-diary/infra"
 	"github.com/yulog/mi-diary/logic"
@@ -27,34 +25,7 @@ func main() {
 
 	migrate.Do(infra)
 
-	e := echo.New()
-	e.Use(middleware.Logger())
-	// e.Use(middleware.Gzip())
-	e.Use(middleware.Recover())
-	e.Validator = server.NewValidator()
-
-	e.GET("/", srv.RootHandler)
-	e.GET("/callback/:host", srv.CallbackHandler)
-	e.GET("/manage", srv.ManageHandler)
-
-	job := e.Group("/job")
-	job.GET("", srv.JobHandler)
-	job.GET("/progress", srv.JobProgressHandler)
-	job.POST("/start", srv.JobStartHandler)
-
-	profiles := e.Group("/profiles")
-	profiles.GET("", srv.NewProfilesHandler)
-	profiles.POST("", srv.AddProfileHandler)
-
-	profile := profiles.Group("/:profile")
-	profile.GET("", srv.HomeHandler)
-	profile.GET("/reactions/:name", srv.ReactionsHandler)
-	profile.GET("/hashtags/:name", srv.HashTagsHandler)
-	profile.GET("/users/:name", srv.UsersHandler)
-	profile.GET("/files", srv.FilesHandler)
-	profile.GET("/notes", srv.NotesHandler)
-	profile.GET("/archives", srv.ArchivesHandler)
-	profile.GET("/archives/:date", srv.ArchiveNotesHandler)
+	e := srv.NewRouter()
 
 	// TODO: context良く分からない
 	go logic.JobProcesser(context.Background())
---
+++ server/router.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func (srv *Server) NewRouter() *echo.Echo {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	// e.Use(middleware.Gzip())
+	e.Use(middleware.Recover())
+	e.Validator = NewValidator()
+
+	e.GET("/", srv.RootHandler)
+	e.GET("/callback/:host", srv.CallbackHandler)
+	e.GET("/manage", srv.ManageHandler)
+
+	job := e.Group("/job")
+	job.GET("", srv.JobHandler)
+	job.GET("/progress", srv.JobProgressHandler)
+	job.POST("/start", srv.JobStartHandler)
+
+	profiles := e.Group("/profiles")
+	profiles.GET("", srv.NewProfilesHandler)
+	profiles.POST("", srv.AddProfileHandler)
+
+	profile := profiles.Group("/:profile")
+	profile.GET("", srv.HomeHandler)
+	profile.GET("/reactions/:name", srv.ReactionsHandler)
+	profile.GET("/hashtags/:name", srv.HashTagsHandler)
+	profile.GET("/users/:name", srv.UsersHandler)
+	profile.GET("/files", srv.FilesHandler)
+	profile.GET("/notes", srv.NotesHandler)
+	profile.GET("/archives", srv.ArchivesHandler)
+	profile.GET("/archives/:date", srv.ArchiveNotesHandler)
+
+	return e
+}
