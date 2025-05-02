fd90e9db3ae871fb21f6bd14b1551c5258a9aab3
yulog <29723448+yulog@users.noreply.github.com>
2025-05-02 22:20:19 +0900

    refactor: deprecated

--- util/pg/pg.go
+++ util/pg/pg.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 )
 
+// Deprecated: use pagination
 type Pager struct {
 	page     int
 	lastOnce sync.Once
@@ -15,6 +16,7 @@ type Pager struct {
 	limit int
 }
 
+// Deprecated: use pagination
 func New(total int) *Pager {
 	return &Pager{
 		total: total,
@@ -22,6 +24,7 @@ func New(total int) *Pager {
 	}
 }
 
+// Deprecated: use pagination
 func (p *Pager) Page(page int) int {
 	if page < 1 {
 		p.page = 1
@@ -31,14 +34,17 @@ func (p *Pager) Page(page int) int {
 	return p.page
 }
 
+// Deprecated: use pagination
 func (p *Pager) Limit() int {
 	return p.limit
 }
 
+// Deprecated: use pagination
 func (p *Pager) Offset() int {
 	return p.limit * (p.page - 1)
 }
 
+// Deprecated: use pagination
 func (p *Pager) Last() int {
 	p.lastOnce.Do(func() { // TODO: 特にこうする意味もない気がする
 		p.last = int(math.Ceil(float64(p.total) / float64(p.limit)))
@@ -46,10 +52,12 @@ func (p *Pager) Last() int {
 	return p.last
 }
 
+// Deprecated: use pagination
 func (p *Pager) Prev() int {
 	return p.page - 1
 }
 
+// Deprecated: use pagination
 func (p *Pager) Next() int {
 	return p.page + 1
 }
