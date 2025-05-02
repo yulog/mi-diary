f150607274b87cad318bf1b916c1c2bab22dd993
yulog <29723448+yulog@users.noreply.github.com>
2025-05-02 22:16:32 +0900

    chore: 未使用のためコメントアウト

--- logic/emoji.go
+++ logic/emoji.go
@@ -1,56 +1,56 @@
 package logic
 
-import (
-	"bytes"
-	"context"
-	"log/slog"
-	"net/url"
+// import (
+// 	"bytes"
+// 	"context"
+// 	"log/slog"
+// 	"net/url"
 
-	"github.com/goccy/go-json"
-	mi "github.com/yulog/miutil"
-)
+// 	"github.com/goccy/go-json"
+// 	mi "github.com/yulog/miutil"
+// )
 
-type EmojiLogic interface {
-	GetOne(ctx context.Context, profile, name string)
-}
+// type EmojiLogic interface {
+// 	// GetOne(ctx context.Context, profile, name string)
+// }
 
-type emojiLogic struct {
-	repo       Repositorier
-	emojiRepo  EmojiRepositorier
-	configRepo ConfigRepositorier
-}
+// type emojiLogic struct {
+// repo Repositorier
+// emojiRepo  EmojiRepositorier
+// configRepo ConfigRepositorier
+// }
 
-func NewEmoji(r Repositorier) EmojiLogic {
-	return &emojiLogic{repo: r}
-}
+// func NewEmoji(r Repositorier) EmojiLogic {
+// 	return &emojiLogic{repo: r}
+// }
 
-func (l emojiLogic) GetOne(ctx context.Context, profile, name string) {
-	host, err := l.configRepo.GetProfileHost(profile)
-	if err != nil {
-		return
-	}
-	body := map[string]any{
-		"name": name,
-	}
+// func (l emojiLogic) GetOne(ctx context.Context, profile, name string) {
+// 	host, err := l.configRepo.GetProfileHost(profile)
+// 	if err != nil {
+// 		return
+// 	}
+// 	body := map[string]any{
+// 		"name": name,
+// 	}
 
-	// https://host.tld/api/emoji
-	u := (&url.URL{
-		Scheme: "https",
-		Host:   host,
-	}).
-		JoinPath("api", "emoji").String()
-	buf := bytes.NewBuffer(nil)
-	json.NewEncoder(buf).Encode(body)
+// 	// https://host.tld/api/emoji
+// 	u := (&url.URL{
+// 		Scheme: "https",
+// 		Host:   host,
+// 	}).
+// 		JoinPath("api", "emoji").String()
+// 	buf := bytes.NewBuffer(nil)
+// 	json.NewEncoder(buf).Encode(body)
 
-	emoji, err := mi.Post2[mi.Emoji](u, buf)
-	if err != nil {
-		slog.Error(err.Error())
-	}
+// 	emoji, err := mi.Post2[mi.Emoji](u, buf)
+// 	if err != nil {
+// 		slog.Error(err.Error())
+// 	}
 
-	res, err := l.emojiRepo.GetByName(ctx, profile, name)
-	if err != nil {
-		// TODO: エラー処理
-		slog.Error(err.Error())
-	}
-	l.emojiRepo.UpdateByPKWithImage(ctx, profile, res.ID, emoji)
-}
+// 	res, err := l.emojiRepo.GetByName(ctx, profile, name)
+// 	if err != nil {
+// 		// TODO: エラー処理
+// 		slog.Error(err.Error())
+// 	}
+// 	l.emojiRepo.UpdateByPKWithImage(ctx, profile, res.ID, emoji)
+// }
